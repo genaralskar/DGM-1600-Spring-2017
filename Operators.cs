98c5fcc53f4c471db524fcdd812f1b2acf736f2a
Collin Anderson <genaralskar1@gmail.com>
2017-02-09 19:06:28 -0700

    add operators.cs

---
+++ Operators.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Operators : MonoBehaviour {
+
+	public int one = 5;
+	public int two = 3;
+
+	// Use this for initialization
+	void Start () {
+		// Add one to two
+		Debug.Log(one + two);
+		// Subtract one from two
+		Debug.Log(one - two);
+		// Multiply one times two
+		Debug.Log(one * two);
+		// Divide one by two
+		Debug.Log(one / two);
+		// Modulus
+		Debug.Log(one % two);
+		// Increment one
+		Debug.Log(++one);
+		// Decrement one
+		Debug.Log(--one);
+		//Square one
+		Debug.Log(one * one);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
